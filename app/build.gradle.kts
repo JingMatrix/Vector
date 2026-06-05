88afae9ee5440b037e6c49a5550c9ceaa7624d64
Łukasz Kuchta <kuchtapc2015@gmail.com>
2026-06-05 22:13:16 +0200

    Update build.gradle.kts
    Correct hex values for colors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,25 +83,25 @@ materialThemeBuilder {
     themes {
         for ((name, color) in
             listOf(
-                "Red" to "F44336",
-                "Pink" to "E91E63",
-                "Purple" to "9C27B0",
+                "Red" to "FF0000",
+                "Pink" to "FFC0CB",
+                "Purple" to "800080",
                 "DeepPurple" to "673AB7",
                 "Indigo" to "3F51B5",
                 "Blue" to "2196F3",
                 "LightBlue" to "03A9F4",
                 "Cyan" to "00BCD4",
                 "Teal" to "009688",
-                "Green" to "4FAF50",
-                "LightGreen" to "8BC3A4",
+                "Green" to "4CAF50",
+                "LightGreen" to "8BC34A",
                 "Lime" to "CDDC39",
                 "Yellow" to "FFEB3B",
                 "Amber" to "FFC107",
                 "Orange" to "FF9800",
                 "DeepOrange" to "FF5722",
                 "Brown" to "795548",
-                "BlueGrey" to "607D8F",
-                "Sakura" to "FF9CA8",
+                "BlueGrey" to "607D8B",
+                "Sakura" to "FFB7C5",
             )) {
             create("Material$name") {
                 lightThemeFormat = "ThemeOverlay.Light.%s"
